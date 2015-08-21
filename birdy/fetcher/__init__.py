68781dd8e6de6069c24a729d0f5910bbb871bc0a
Julien Maupetit <julien@tailordev.fr>
2015-08-21 17:05:23 +0200

    Fetch data format only if asked to

--- birdy/fetcher/__init__.py
+++ birdy/fetcher/__init__.py
@@ -7,32 +7,37 @@ from .pdb import generate_pdb_set
 def main(output_path, formats=None, use_cache=True):
 
     # PDB
-    generate_pdb_set(
-        output_path,
-        formats.get('PDB'),
-        input_ids=None,
-        use_cache=use_cache
-    )
+    if sum(formats.get('PDB').values()):
+
+        generate_pdb_set(
+            output_path,
+            formats.get('PDB'),
+            input_ids=None,
+            use_cache=use_cache
+        )
 
     # NCBI
-    for db in config.NCBI_DATABASES:
+    if sum(formats.get('NCBI').values()):
 
-        generate_ncbi_set(
-            output_path,
-            db,
-            formats.get('NCBI'),
-            input_ids=None,
-            use_cache=use_cache
-        )
+        for db in config.NCBI_DATABASES:
 
+            generate_ncbi_set(
+                output_path,
+                db,
+                formats.get('NCBI'),
+                input_ids=None,
+                use_cache=use_cache
+            )
 
     # KEGG
-    for db in config.KEGG_DATABASES:
+    if sum(formats.get('KEGG').values()):
 
-        generate_kegg_set(
-            output_path,
-            db,
-            formats.get('KEGG'),
-            input_ids=None,
-            use_cache=use_cache
-        )
+        for db in config.KEGG_DATABASES:
+
+            generate_kegg_set(
+                output_path,
+                db,
+                formats.get('KEGG'),
+                input_ids=None,
+                use_cache=use_cache
+            )
