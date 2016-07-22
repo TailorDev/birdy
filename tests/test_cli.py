d11aaa5e1f344c77eba097805a0e056f1f92e9fe
Julien Maupetit <julien@tailordev.fr>
2016-07-22 12:19:19 +0200

    Improve fasta testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,7 @@
 import pytest
 import subprocess
 
-from os import environ
+from os import environ, listdir, path
 
 
 @pytest.fixture
@@ -19,5 +19,18 @@ def run():
 
 
 def test_fasta(entrez, tmpdir, run):
+    """Test fasta files fetching from NCBI databases"""
+
     args = ['--fasta', '1', tmpdir.strpath]
+    fasta_dir = path.join(tmpdir.strpath, 'fasta')
+
     assert run(args) == 0
+    assert path.exists(fasta_dir)
+
+    for biotype in ('nucleotide', 'protein'):
+        fasta_type_dir = path.join(fasta_dir, biotype)
+        assert path.exists(fasta_type_dir)
+        fasta_type_files = listdir(fasta_type_dir)
+        assert len(fasta_type_files) == 1
+        assert fasta_type_files[0][-6:] == '.fasta'
+        assert path.getsize(path.join(fasta_type_dir, fasta_type_files[0])) > 0
